480df21fd4a46b0037e0489d214095646689d6c9
changhr2013 <changhr2013@outlook.com>
2018-11-30 00:43:13 +0800

    [mod]Feign 自定义注解

--- microservice-consumer-movie/src/main/java/com/changhr/cloud/consumer/movie/user/feign/UserFeignClient.java
+++ microservice-consumer-movie/src/main/java/com/changhr/cloud/consumer/movie/user/feign/UserFeignClient.java
@@ -1,6 +1,9 @@
 package com.changhr.cloud.consumer.movie.user.feign;
 
+import com.changhr.cloud.consumer.movie.config.FeignConfiguration;
 import com.changhr.cloud.consumer.movie.user.entity.User;
+import feign.Param;
+import feign.RequestLine;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +12,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * microservice-provider-user 的 Feign 接口
  *
+ * 2. 使用 @FeignClient 的 configuration 属性，指定 feign 的配置类
+ *
  * @author changhr
  */
-@FeignClient(name = "microservice-provider-user")
+@FeignClient(name = "microservice-provider-user", configuration = FeignConfiguration.class)
 public interface UserFeignClient {
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    User findById(@PathVariable("id") Long id);
+//    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+//    User findById(@PathVariable("id") Long id);
+
+    /**
+     * 使用 feign 自带的注解 @RequestLine
+     * @param id    用户 Id
+     * @return User 用户信息
+     */
+    @RequestLine("GET /{id}")
+    User findById(@Param("id") Long id);
 }
