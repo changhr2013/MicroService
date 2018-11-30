8572db5233a422bfa3f2dc855e40588544c7384a
常浩然 <changhr@sense.com.cn>
2018-11-30 20:28:00 +0800

    [add]添加spring cloud config

---
+++ microservice-config-server/src/main/java/com/changhr/cloud/config/server/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.changhr.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Spring Cloud 配置中心
+ *
+ * @author changhr
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
