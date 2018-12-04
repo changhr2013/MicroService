03d833d8be4f988cd6b21af4dadd8af1d27cbc36
常浩然 <changhr@sense.com.cn>
2018-12-04 19:56:02 +0800

    [mod]修复文件上传问题和turbine注册问题

--- microservice-hystrix-turbine/src/main/java/com/changhr/cloud/hystrix/turbine/HystrixTurbineApplication.java
+++ microservice-hystrix-turbine/src/main/java/com/changhr/cloud/hystrix/turbine/HystrixTurbineApplication.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 @SpringBootApplication
 //@EnableTurbine
 @EnableTurbineStream
-@EnableCircuitBreaker
+//@EnableCircuitBreaker
 @EnableDiscoveryClient
 public class HystrixTurbineApplication {
 
